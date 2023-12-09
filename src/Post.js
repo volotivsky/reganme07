61118b29f76085246b201ff5824cfa374ef6ab1f
volotivsky <115136715+volotivsky@users.noreply.github.com>
2023-12-09 16:07:46 +0300

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,6 @@
 
 import Aboutpost from './Components/Aboutpost';
 const Post = () => {
-    axios.interceptors.request.use(function (config) {
-        const token = window.localStorage.getItem('token')
-        config.headers.Authorization =  token;     
-        return config;
-    });
-    
     return (
         <div>
             <Aboutpost/>
